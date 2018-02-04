d85e17a698ffa1bed1de2fafba88be7e9668cfcd
Benedikt Müssig <benedikt.muessig@gmail.com>
2018-02-04 21:25:03 +0100

    Implemented Track with writing support

--- SharpSprint/Elements/Track.cs
+++ SharpSprint/Elements/Track.cs
@@ -2,11 +2,32 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using SharpSprint.Primitives;
+using SharpSprint.IO;
 
 namespace SharpSprint.Elements
 {
-    public class Track : Entity
+    public class Track : Element
     {
+        // Required parameters
+        public Layer Layer { get; set; }
+        public Distance Width { get; set; }
+        public List<Position> Path { get; set; }
+
+        // Optional parameters
+        public Distance Clear { get; set; } // 4000
+        public bool Cutout { get; set; } // False
+        public bool Soldermask { get; set; } // False
+        public bool FlatStart { get; set; } // False
+        public bool FlatEnd { get; set; } // False
+
+        // Default optional parameters
+        private const uint ClearDefault = 4000;
+        private const bool CutoutDefault = false;
+        private const bool SoldermaskDefault = false;
+        private const bool FlatStartDefault = false;
+        private const bool FlatEndDefault = false;
+
         public bool Read(IO.Token[][] Tokens, ref uint Pointer)
         {
             throw new NotImplementedException();
@@ -14,7 +35,62 @@ namespace SharpSprint.Elements
 
         public bool Write(out IO.Token[][] Tokens)
         {
-            throw new NotImplementedException();
+            TokenWriter writer = new TokenWriter();
+            Tokens = null;
+
+            // Write the type first
+            writer.Write(new Token("TRACK", Token.IndentTransition.None));
+
+            // Now write the required values
+            // Layer
+            if (Layer >= Layer.CopperTop && Layer <= Layer.Mechanical)
+                writer.Write(new Token("LAYER", (ulong)Layer));
+            else
+                return false;
+
+            // Width
+            writer.Write(new Token("WIDTH", Width.Value));
+
+            // Points
+            if (Path.Count >= 2)
+            {
+                for (int counter = 0; counter < Path.Count; counter++)
+                {
+                    if (counter > 0)
+                    {
+                        if ((Path[counter].X.Value == Path[counter - 1].X.Value) && (Path[counter].Y.Value == Path[counter - 1].Y.Value))
+                            continue;
+                    }
+                    writer.Write(new Token(string.Format("P{0}", counter), Path[counter].X.Value, Path[counter].Y.Value));
+                    counter++;
+                }
+            }
+            else
+                return false;
+
+            // Then write the optional values
+            // Clear
+            if (Clear.Value != ClearDefault)
+                writer.Write(new Token("CLEAR", Clear.Value));
+
+            // Cutout
+            if (Cutout != CutoutDefault)
+                writer.Write(new Token("CUTOUT", Cutout));
+
+            // Soldermask
+            if (Soldermask != SoldermaskDefault)
+                writer.Write(new Token("SOLDERMASK", Soldermask));
+
+            // FlatStart
+            if (FlatStart != FlatStartDefault)
+                writer.Write(new Token("FLATSTART", FlatStart));
+
+            // FlatEnd
+            if (FlatEnd != FlatEndDefault)
+                writer.Write(new Token("FLATEND", FlatEnd));
+
+            Tokens = writer.Compile();
+            return true;
         }
     }
 }
