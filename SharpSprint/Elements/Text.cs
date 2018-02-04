b432f4cf8af5fb87ea84dc725d5e19c28b9eaaf0
Benedikt Müssig <benedikt.muessig@gmail.com>
2018-02-04 14:41:35 +0100

    Added implementation for Text

--- SharpSprint/Elements/Text.cs
+++ SharpSprint/Elements/Text.cs
@@ -2,11 +2,39 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using SharpSprint.Primitives;
+using SharpSprint.IO;
 
 namespace SharpSprint.Elements
 {
-    public class Text : Entity
+    public class Text : Element
     {
+        // Required parameters
+        public Layer Layer { get; set; }
+        public Position Position { get; set; }
+        public string Content { get; set; }
+        public Size Height { get; set; }
+
+        // Optional parameters
+        public Size Clear { get; set; } // 4000
+        public bool Cutout { get; set; } // False
+        public bool Soldermask { get; set; } // False
+        public TextStyle Style { get; set; } // Normal
+        public TextThickness Thickness { get; set; } // Normal
+        public CoarseAngle Rotation { get; set; } // 0
+        public bool MirrorHorizontal { get; set; } // False
+        public bool MirrorVertical { get; set; } // False
+
+        // Default optional parameters
+        private const uint ClearDefault = 4000;
+        private const bool CutoutDefault = false;
+        private const bool SoldermaskDefault = false;
+        private const TextStyle StyleDefault = TextStyle.Normal;
+        private const TextThickness ThicknessDefault = TextThickness.Normal;
+        private const uint RotationDefault = 0;
+        private const bool MirrorHorizontalDefault = false;
+        private const bool MirrorVerticalDefault = false;
+
         public bool Read(IO.Token[][] Tokens, ref uint Pointer)
         {
             throw new NotImplementedException();
@@ -14,7 +42,77 @@ namespace SharpSprint.Elements
 
         public bool Write(out IO.Token[][] Tokens)
         {
-            throw new NotImplementedException();
+            TokenWriter writer = new TokenWriter();
+            Tokens = null;
+
+            // Write the type first
+            writer.Write(new Token("CIRCLE", Token.IndentTransition.None));
+
+            // Now write the required values
+            // Layer
+            if (Layer >= Layer.CopperTop && Layer <= Layer.Mechanical)
+                writer.Write(new Token("LAYER", (ulong)Layer));
+            else
+                return false;
+
+            // Position
+            writer.Write(new Token("POS", Position.X.Value, Position.Y.Value));
+
+            // Content
+            writer.Write(new Token("TEXT", Content));
+
+            // Height
+            writer.Write(new Token("HEIGHT", Height.Value));
+
+            // Then write the optional values
+            // Clear
+            if (Clear.Value != ClearDefault)
+                writer.Write(new Token("CLEAR", Clear.Value));
+
+            // Cutout
+            if (Cutout != CutoutDefault)
+                writer.Write(new Token("CUTOUT", Cutout));
+
+            // Soldermask
+            if (Soldermask != SoldermaskDefault)
+                writer.Write(new Token("SOLDERMASK", Soldermask));
+
+            // Style
+            if (Style != StyleDefault)
+                writer.Write(new Token("STYLE", (ulong)Style));
+
+            // Thickness
+            if (Thickness != ThicknessDefault)
+                writer.Write(new Token("THICKNESS", (ulong)Thickness));
+
+            // Rotation
+            if (Rotation.Value != RotationDefault)
+                writer.Write(new Token("ROTATION", Rotation.Value));
+
+            // MirrorHorizontal
+            if (MirrorHorizontal != MirrorHorizontalDefault)
+                writer.Write(new Token("MIRROR_HORZ", MirrorHorizontal));
+
+            // MirrorVertical
+            if (MirrorVertical != MirrorVerticalDefault)
+                writer.Write(new Token("MIRROR_VERT", MirrorVertical));
+
+            Tokens = writer.Compile();
+            return true;
+        }
+
+        public enum TextStyle : byte
+        {
+            Narrow = 0,
+            Normal = 1,
+            Wide = 2
+        }
+
+        public enum TextThickness : byte
+        {
+            Thin = 0,
+            Normal = 1,
+            Thick = 2
         }
     }
 }
