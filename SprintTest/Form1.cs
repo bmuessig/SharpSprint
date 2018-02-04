41106c7ef44ba9d7681cbbd505e362b3b720fe62
Benedikt Müssig <benedikt.muessig@gmail.com>
2018-02-04 17:13:00 +0100

    Added first working test

--- SprintTest/Form1.cs
+++ SprintTest/Form1.cs
@@ -6,6 +6,8 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using SharpSprint.Elements;
+using SharpSprint.IO;
 
 namespace SprintTest
 {
@@ -14,6 +16,31 @@ namespace SprintTest
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Group Test = new Group();
+            Circle C1 = new Circle(SharpSprint.Layer.CopperTop, SharpSprint.Primitives.Size.FromMillimeters(1),
+                SharpSprint.Primitives.Position.FromMillimeters(10, 10), SharpSprint.Primitives.Size.FromMillimeters(20));
+            Circle C2 = new Circle(SharpSprint.Layer.CopperTop, SharpSprint.Primitives.Size.FromMillimeters(3),
+                SharpSprint.Primitives.Position.FromMillimeters(15, 20), SharpSprint.Primitives.Size.FromMillimeters(30));
+            Test.Entities.Add(C1);
+            Test.Entities.Add(C2);
+
+            Token[][] lines;
+            ushort indent = 0;
+            string output;
+
+            if (Test.Write(out lines))
+            {
+                if (Compiler.CompileBlock(lines, ref indent, out output))
+                    textBox1.Text = output;
+                else
+                    textBox1.Text = "Compile FAIL!";
+            }
+            else
+                textBox1.Text = "Write FAIL!";
         }
     }
 }
