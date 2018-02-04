309dbb520458db78fccf2b83bab9e08751d20633
Benedikt Müssig <benedikt.muessig@gmail.com>
2018-02-04 14:04:32 +0100

    Implemented Circle writing

--- SharpSprint/Elements/Circle.cs
+++ SharpSprint/Elements/Circle.cs
@@ -23,6 +23,14 @@ namespace SharpSprint.Elements
         public FineAngle Stop { get; set; } // 0
         public bool Fill { get; set; } // False
 
+        // Default optional parameters
+        private const uint ClearDefault = 4000;
+        private const bool CutoutDefault = false;
+        private const bool SoldermaskDefault = false;
+        private const uint StartDefault = 0;
+        private const uint StopDefault = 0;
+        private const bool FillDefault = false;
+
         public bool Read(Token[][] Tokens, ref uint Pointer)
         {
             throw new NotImplementedException();
@@ -30,7 +38,56 @@ namespace SharpSprint.Elements
         
         public bool Write(out Token[][] Tokens)
         {
-            throw new NotImplementedException();
+            TokenWriter writer = new TokenWriter();
+            Tokens = null;
+
+            // Write the type first
+            writer.Write(new Token("CIRCLE", Token.IndentTransition.None));
+
+            // Now write the required values
+            // Layer
+            if (Layer >= Layer.CopperTop && Layer <= Layer.Mechanical)
+                writer.Write(new Token("LAYER", (ulong)Layer));
+            else
+                return false;
+
+            // Width
+            writer.Write(new Token("WIDTH", Width.Value));
+
+            // Center
+            writer.Write(new Token("CENTER", Center.X.Value, Center.Y.Value));
+
+            // Radius
+            writer.Write(new Token("RADIUS", Radius.Value));
+
+            // Then write the optional values
+
+            // Clear
+            if (Clear.Value != ClearDefault)
+                writer.Write(new Token("CLEAR", Clear.Value));
+
+            // Cutout
+            if (Cutout != CutoutDefault)
+                writer.Write(new Token("CUTOUT", Cutout));
+
+            // Soldermask
+            if (Soldermask != SoldermaskDefault)
+                writer.Write(new Token("SOLDERMASK", Soldermask));
+
+            // Start
+            if (Start.Value != StartDefault)
+                writer.Write(new Token("START", Start.Value));
+
+            // Stop
+            if (Stop.Value != StopDefault)
+                writer.Write(new Token("STOP", Stop.Value));
+            
+            // Fill
+            if (Fill != FillDefault)
+                writer.Write(new Token("FILL", Fill));
+
+            Tokens = writer.Compile();
+            return true;
         }
     }
 }
