48c469c2e9a96d622fe2e6c0fa3e7bba0892bbd1
Benedikt Müssig <benedikt.muessig@gmail.com>
2018-02-04 16:54:26 +0100

    Improved Rectangle and Position

--- SharpSprint/Primitives/Position.cs
+++ SharpSprint/Primitives/Position.cs
@@ -9,5 +9,21 @@ namespace SharpSprint.Primitives
     {
         public Size X;
         public Size Y;
+
+        public Position(Size X, Size Y)
+        {
+            this.X = X;
+            this.Y = Y;
+        }
+
+        public static Position FromMillimeters(float X, float Y)
+        {
+            return new Position(Size.FromMillimeters(X), Size.FromMillimeters(Y));
+        }
+
+        public static Position FromInches(float X, float Y)
+        {
+            return new Position(Size.FromInches(X), Size.FromInches(Y));
+        }
     }
 }
--- SharpSprint/Primitives/Rectangle.cs
+++ SharpSprint/Primitives/Rectangle.cs
@@ -9,5 +9,21 @@ namespace SharpSprint.Primitives
     {
         public Size Width;
         public Size Height;
+
+        public Rectangle(Size Width, Size Height)
+        {
+            this.Width = Width;
+            this.Height = Height;
+        }
+
+        public static Rectangle FromMillimeters(float Width, float Height)
+        {
+            return new Rectangle(Size.FromMillimeters(Width), Size.FromMillimeters(Height));
+        }
+
+        public static Rectangle FromInches(float Width, float Height)
+        {
+            return new Rectangle(Size.FromInches(Width), Size.FromInches(Height));
+        }
     }
 }
