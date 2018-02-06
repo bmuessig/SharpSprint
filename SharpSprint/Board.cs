3af44cd4736a2fa254428e820ab2656793c6ef66
Benedikt Müssig <benedikt.muessig@gmail.com>
2018-02-06 06:50:35 +0100

    Changed Canvas into Board

---
+++ SharpSprint/Board.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SharpSprint.Elements;
+using SharpSprint.IO;
+
+namespace SharpSprint
+{
+    public class Board
+    {
+        private Group Entities;
+
+        public List<Entity> Canvas
+        {
+            get { return Entities.Entities; }
+        }
+
+        public Board()
+        {
+            Entities = new Group();
+        }
+
+        public bool Compile(out string Result)
+        {
+            Result = null;
+
+            Token[][] tokens;
+            if (!Entities.Write(out tokens))
+                return false;
+
+            ushort indent = 0;
+            return Compiler.CompileBlock(tokens, ref indent, out Result);
+        }
+    }
+}
