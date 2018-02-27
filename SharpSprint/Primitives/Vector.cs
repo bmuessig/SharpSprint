c401fc02be90bcfb1c89e61e788325efb27cea3b
Benedikt Müssig <benedikt.muessig@gmail.com>
2018-02-28 00:10:58 +0100

    Added Angles

--- SharpSprint/Primitives/Vector.cs
+++ SharpSprint/Primitives/Vector.cs
@@ -34,6 +34,58 @@ namespace SharpSprint.Primitives
             }
         }
 
+        public decimal Angle
+        {
+            get
+            {
+                // Errors
+                if (X == null)
+                    X = new Distance();
+                if (Y == null)
+                    Y = new Distance();
+                if (X.Value == 0 || Y.Value == 0)
+                    return 0;
+
+                return (decimal)((180 * Math.Atan(Y.Value / X.Value)) / Math.PI);
+            }
+
+            set
+            {
+                if (X == null)
+                    X = new Distance();
+                if (Y == null)
+                    Y = new Distance();
+                // Make sure, we have got one valid value
+                if ((X.Value == 0 && !AdjustX) || (Y.Value == 0 && AdjustX))
+                    return; // TODO: Handle this error better
+
+                // Calculate the remaining side
+                // Note, that C#'s trig functions are using radians
+                if(AdjustX)
+                    X = new Distance((ulong)Math.Round(Y.Value / Math.Tan(((double)value * Math.PI) / 180), 0));
+                else
+                    Y = new Distance((ulong)Math.Round(X.Value * Math.Tan(((double)value * Math.PI) / 180), 0));
+            }
+        }
+
+        public FineAngle AngleFine
+        {
+            get { return FineAngle.FromAngle(this.Angle); }
+            set { this.Angle = value.Angle; }
+        }
+
+        public CoarseAngle AngleCoarse
+        {
+            get { return CoarseAngle.FromAngle(this.Angle); }
+            set { this.Angle = value.Angle; }
+        }
+
+        public IntegerAngle AngleInteger
+        {
+            get { return IntegerAngle.FromAngle(this.Angle); }
+            set { this.Angle = value.Angle; }
+        }
+
         public Vector(Distance X, Distance Y)
         {
             this.X = X;
