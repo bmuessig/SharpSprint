8d124b094b051be174d5f7c9013f4e7e02a77ca9
Benedikt Müssig <benedikt.muessig@gmail.com>
2018-02-27 23:42:47 +0100

    Implemented Vector Length and Math

--- SharpSprint/Primitives/Vector.cs
+++ SharpSprint/Primitives/Vector.cs
@@ -7,11 +7,32 @@ namespace SharpSprint.Primitives
 {
     public class Vector
     {
-        //private Distance 
-
         public Distance X { get; set; }
         
         public Distance Y { get; set; }
+
+        public bool AdjustX { get; set; }
+
+        public Distance Length
+        {
+            get
+            {
+                return new Distance((ulong)(Math.Sqrt(X.Value * X.Value + Y.Value * Y.Value)));
+            }
+
+            set
+            {
+                if (X == null)
+                    X = new Distance();
+                if (Y == null)
+                    Y = new Distance();
+
+                if(AdjustX)
+                    X = new Distance((ulong)(Math.Sqrt(value.Value * value.Value - Y.Value * Y.Value)));
+                else
+                    Y = new Distance((ulong)(Math.Sqrt(value.Value * value.Value - X.Value * X.Value)));
+            }
+        }
 
         public Vector(Distance X, Distance Y)
         {
@@ -27,6 +48,31 @@ namespace SharpSprint.Primitives
         public static Vector FromInches(decimal X, decimal Y)
         {
             return new Vector(Distance.FromInches(X), Distance.FromInches(Y));
+        }
+
+        public static Vector operator +(Vector A, Vector B)
+        {
+            return new Vector(A.X + B.X, A.Y + B.Y);
+        }
+
+        public static Vector operator -(Vector A, Vector B)
+        {
+            return new Vector(A.X - B.X, A.Y - B.Y);
+        }
+
+        public static Vector operator *(Vector V, uint N)
+        {
+            return new Vector(V.X * N, V.Y * N);
+        }
+
+        public static Vector operator *(uint N, Vector V)
+        {
+            return (V * N);
+        }
+
+        public static Vector operator /(Vector V, uint N)
+        {
+            return new Vector(V.X / N, V.Y / N);
         }
     }
 }
