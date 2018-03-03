61675d4b698a74bcd0d49075820ded591a593d6a
Benedikt Müssig <benedikt.muessig@gmail.com>
2018-03-03 17:55:29 +0100

    Started with plotting functionality

---
+++ SharpSprintPlot/Plotter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SharpSprint.Elements;
+using System.Drawing;
+
+namespace SharpSprintPlot
+{
+    public static class Plotter
+    {
+        public static Bitmap Draw(Group Group)
+        {
+            return null;
+        }
+
+        public static Bitmap Draw(Element Element)
+        {
+            if (Element.GetType() == typeof(Circle)) ;
+            return null;
+        }
+    }
+}
---
+++ SharpSprintPlot/SprintPlot.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace SharpSprintPlot
+{
+    partial class SprintPlot
+    {
+        /// <summary> 
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Komponenten-Designer generierter Code
+
+        /// <summary> 
+        /// Erforderliche Methode für die Designerunterstützung. 
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Canvas = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Canvas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Canvas
+            // 
+            this.Canvas.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Canvas.Location = new System.Drawing.Point(0, 0);
+            this.Canvas.Name = "Canvas";
+            this.Canvas.Size = new System.Drawing.Size(150, 150);
+            this.Canvas.TabIndex = 0;
+            this.Canvas.TabStop = false;
+            // 
+            // SprintPlot
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.Canvas);
+            this.Name = "SprintPlot";
+            ((System.ComponentModel.ISupportInitialize)(this.Canvas)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Canvas;
+    }
+}
