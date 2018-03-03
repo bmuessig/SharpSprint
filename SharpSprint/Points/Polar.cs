20aa570c235180a1ff87d911bd6454fae3ef7dc2
Benedikt Müssig <benedikt.muessig@gmail.com>
2018-03-03 17:55:10 +0100

    Added Polar and RefPoint

---
+++ SharpSprint/Points/Polar.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SharpSprint.Primitives;
+
+namespace SharpSprint.Points
+{
+    public class Polar : Point
+    {
+        public Point Origin { get; set; }
+
+        public Distance Radius { get; set; }
+
+        public decimal Angle { get; set; }
+
+        public FineAngle AngleFine
+        {
+            get { return FineAngle.FromAngle(this.Angle); }
+            set { this.Angle = value.Angle; }
+        }
+
+        public CoarseAngle AngleCoarse
+        {
+            get { return CoarseAngle.FromAngle(this.Angle); }
+            set { this.Angle = value.Angle; }
+        }
+
+        public IntegerAngle AngleInteger
+        {
+            get { return IntegerAngle.FromAngle(this.Angle); }
+            set { this.Angle = value.Angle; }
+        }
+
+        public new Distance X
+        {
+            get
+            {
+                return new Distance((uint)Math.Round(Origin.X.Value + Radius.Value * Math.Cos(((double)Angle * Math.PI) / 180), 0));
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public new Distance Y
+        {
+            get
+            {
+                return new Distance((uint)Math.Round(Origin.Y.Value + Radius.Value * Math.Sin(((double)Angle * Math.PI) / 180), 0));
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
---
+++ SharpSprint/Points/RefPoint.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SharpSprint.Primitives;
+
+namespace SharpSprint.Points
+{
+    public class RefPoint : Point
+    {
+        // You can attach this point to a pad (and it's edges) or just about any other element
+
+        public enum SMDPadRef : byte
+        {
+
+        }
+
+        public enum TrackPointRef : byte
+        {
+
+        }
+    }
+}
